0b74dbeddb951f5fc5406d66f87de59c5d76a442
Geol2 <big9401@gmail.com>
2023-05-10 18:15:22 +0900

    ADD : 저장 함수 추가

--- index.php
+++ index.php
@@ -3,13 +3,11 @@ require 'vendor/autoload.php';
 
 use File\StorageClient;
 
-
-$file = new CURLFile('/', 'txt/plain', 'tes.txt');
+$bucket = $_POST['bucket'];
+$stoken = $_POST['stoken'];
+$folder = $_POST['folder'];
+$fileData = $_FILES['file_data'];
 
 // 서비스에서 사용할 로직
 $client = new StorageClient();
-try {
-    $client->upload("cashmoa", "kaNuStnkcNW1kwn", "a5Es7usLtR", $file);
-} catch (\GuzzleHttp\Exception\GuzzleException $e) {
-
-}
+$client->upload($bucket, $stoken, $folder, $fileData);
--- src/File/StorageClient.php
+++ src/File/StorageClient.php
@@ -3,52 +3,57 @@
 namespace File;
 
 use CURLFile;
-use GuzzleHttp\Exception\GuzzleException;
 
 class StorageClient
 {
     public $url = "";
 
-    /**
-     * @throws GuzzleException
-     */
     public function upload($bucket, $token, $folder, $file) {
 
         $env = getenv("APP_ENV");
         if($env == "production") {
             $this->url = "https://zerostorage.zdev.co.kr/api/upload";
         } else if($env == "local") {
-            $this->url = "https://localhost:8000/api/upload";
+            $this->url = "http://localhost:8000/api/upload";
         } else if($env == "development") {
             $this->url = "https://zerostorage.zdev.co.kr/api/upload";
         } else {
             # 그 외엔 개발 환경
-            $this->url = "https://localhost:8000/api/upload";
+            # $this->url = "https://zerostorage.zdev.co.kr/api/upload";
+            $this->url = "http://localhost:8000/api/upload";
         }
 
-        $file_path = "./";
-        $file_name = "test.txt";
-
-        $post_data = [
-            "bucket" => $bucket,
-            "token" => $token,
-            "folder" => $folder
+        $post = [
+            'bucket' => $bucket,
+            'stoken' => $token,
+            'folder' => $folder,
+            'file_data'=> new CURLFile($file['tmp_name'], $file['type'], $file['name'])
         ];
+        $headers = array("Content-Type:multipart/form-data");
 
-        // cURL 설정
         $curl = curl_init();
+
+        if( strpos($this->url, "https://") !== false ) {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        }
+
         curl_setopt($curl, CURLOPT_URL, $this->url);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_HEADER, $headers);
         curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, array(
-            'file' => new CURLFile($file_path, 'text/plain', $file_name),
-            'data' => http_build_query($post_data),
-        ));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-        // 서버로부터 받은 응답 처리
         $response = curl_exec($curl);
+        if($response === "" || $response === false) {
+            echo "error";
+            return;
+        }
+        $json_data = substr($response, strpos($response, "{"));
+        curl_close($curl);
+        $result = json_decode($json_data);
 
-
+        echo $result->file_url;
     }
 
     public function deleteFullPath($token, $fullPath) {
